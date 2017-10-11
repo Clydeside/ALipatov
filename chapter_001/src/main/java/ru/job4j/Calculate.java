35af98e5183e696565d1bfe24a7940c7bc698724
Clydeside <lipatov.artem@mail.ru>
2017-10-11 21:25:16 +0300

    task 4417

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,26 +1,24 @@
 package ru.job4j;
 
-       /**
-	* Class Calculate.
-	* @param name Your name.
-	* @return Echo plus your name.
-	*/
+/**
+ * Class Calculate.
+ */
 
 public class Calculate {
 	/**
-	  * Method main.
-	  * @param args Your args.
-	  */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	 * Method main.
+	 * @param args Your args.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 
 	/**
-	  * Method echo.
-	  * @param name Your name.
-	  * @return Echo plus your name.
-	  */
-	public String echo(String name) {
-	    return "Echo, echo, echo : " + name;
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+	private String echo(String name) {
+		return "Echo, echo, echo : " + name;
 	}
 }
