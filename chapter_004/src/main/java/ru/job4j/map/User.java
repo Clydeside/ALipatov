2fcb8ba3e896f36343e65f77e0e4de035ff946ec
Clydeside <lipatov.artem@mail.ru>
2018-02-24 13:54:21 +0300

    task Model user #999

---
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,21 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+}
