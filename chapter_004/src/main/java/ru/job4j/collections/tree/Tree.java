b6e4db9becb0da0e08d17ae61c047b0092b0e2a3
Clydeside <lipatov.artem@mail.ru>
2018-03-02 14:20:31 +0300

    task Simple tree #1711

---
+++ chapter_004/src/main/java/ru/job4j/collections/tree/Tree.java
@@ -0,0 +1,41 @@
+package ru.job4j.collections.tree;
+
+import java.util.*;
+
+public class Tree<E extends  Comparable<E>> implements SimpleTree<E> {
+    private Node<E> root;
+
+    public Tree(Node<E> root) {
+        this.root = root;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> result = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> element = data.poll();
+            if (element.eqValue(value)) {
+                result = Optional.of(element);
+                break;
+            }
+            for (Node<E> child : element.leaves()) {
+                data.offer(child);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        Optional<Node<E>> elem = findBy(parent);
+        elem.get().add(new Node(child));
+        return true;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/collections/tree/TreeTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.collections.tree;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TreeTest {
+    @Test
+    public void when6ElFindLastThen6() {
+        Tree<Integer> tree = new Tree<>(new Node<>(1));
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(
+                tree.findBy(6).isPresent(),
+                is(true)
+        );
+    }
+
+    @Test
+    public void when6ElFindNotExitThenOptionEmpty() {
+        Tree<Integer> tree = new Tree<>(new Node<>(1));
+        tree.add(1, 2);
+        assertThat(
+                tree.findBy(7).isPresent(),
+                is(false)
+        );
+    }
+}
