a352fe585f5555ddd5770cdbf90d4a2ca4530367
Clydeside <lipatov.artem@mail.ru>
2018-03-08 08:22:17 +0300

    task ThreadSafe User Storage #1104

---
+++ chapter_004/src/main/java/ru/job4j/concurrency/monitore/User.java
@@ -0,0 +1,25 @@
+package ru.job4j.concurrency.monitore;
+
+import java.util.UUID;
+
+public class User {
+    private int id;
+    private int amount;
+
+    public User(int id, int amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
