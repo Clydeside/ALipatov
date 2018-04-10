b573cf8d48aaf0d44ee228378bd759ff6699176d
Clydeside <lipatov.artem@mail.ru>
2018-04-10 19:18:14 +0300

    task Fixing Non-blocking cache #4741

--- chapter_005/src/main/java/ru/job4j/nonBlockingCache/Cache.java
+++ chapter_005/src/main/java/ru/job4j/nonBlockingCache/Cache.java
@@ -9,41 +9,46 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @ThreadSafe
 public class Cache {
-    @GuardedBy("this")
     private ConcurrentHashMap<Integer, User> map = new ConcurrentHashMap<>();
 
     public User getValue(Integer key) {
-        synchronized (this) {
-            return map.get(key);
-        }
+        return map.get(key);
     }
 
     public boolean add(Integer key, User user) {
-        synchronized (this) {
-            map.put(key, user);
-        }
+        map.put(key, user);
         return true;
     }
 
     public void update(Integer key, User value) {
-        synchronized (this) {
-            map.computeIfPresent(key, (k, oldValue) -> {
-                int oldV = oldValue.getVersion();
-                if (oldV != value.getVersion()) {
-                    throw new OptimisticException();
-                }
-                map.put(k, value);
-                return value;
-            });
+        map.computeIfPresent(key, (k, oldValue) -> {
+            int oldV = oldValue.getVersion();
+            if (oldV != value.getVersion()) {
+                throw new OptimisticException();
+            }
             map.get(key).incrementVersion();
-        }
+            return value;
+        });
     }
 
     public boolean delete(Integer key) {
-        synchronized (this) {
-            map.remove(key);
-        }
+        map.remove(key);
         return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cache cache = (Cache) o;
+
+        return map != null ? map.equals(cache.map) : cache.map == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return map != null ? map.hashCode() : 0;
     }
 
     private class OptimisticException extends RuntimeException {
